dfd285638c02439360e14d27ec054acc10043916
Yahya Mehmet Sağdur <ymsagdur@gmail.com>
2024-07-10 14:46:46 +0300

    feat: add sendSms and sendEmail for notification service with tests

--- src/notification.module.ts
+++ src/notification.module.ts
@@ -1,4 +1,13 @@
+import { EmailModule } from './email/email.module';
 import { Module } from '@nestjs/common';
+import { NotificationService } from './notification.service';
+import { SmsModule } from './sms/sms.module';
 
-@Module({})
+@Module({
+  imports: [EmailModule, SmsModule],
+  providers: [
+    { provide: 'NotificationService', useClass: NotificationService },
+  ],
+  exports: ['NotificationService'],
+})
 export class NotificationModule {}
--- src/notification.service.ts
+++ src/notification.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { SmsService } from './sms/sms.service';
+import { EmailService } from './email/email.service';
+import { ConfigService } from '@nestjs/config';
+import { Sms } from './sms/dto';
+import { Email } from './email/dto';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+  constructor(
+    @Inject('EmailService') private readonly emailService: EmailService,
+    @Inject('SmsService') private readonly smsService: SmsService,
+    private readonly configService: ConfigService,
+  ) {}
+  async sendSms(sms: Sms): Promise<void> {
+    this.smsService.sendSmsAsync({
+      message: sms.message,
+      phoneNumber: sms.phoneNumber,
+    });
+  }
+  async sendEmail(
+    email: Email,
+  ): Promise<void> {
+    return await this.emailService.sendEmailAsync({
+      from: this.configService.get<string>('emailFrom'),
+      to: email.to,
+      subject: email.subject,
+      content: email.content,
+    });
+  }
+}
