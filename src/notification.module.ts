023955ad271825a5f3bb5bfcb4d59491427ee395
Yahya Mehmet Sağdur <ymsagdur@gmail.com>
2024-07-10 18:26:17 +0300

    feat: Add configuration for email and SMS in notification module

--- src/notification.module.ts
+++ src/notification.module.ts
@@ -2,9 +2,18 @@ import { EmailModule } from './email/email.module';
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { SmsModule } from './sms/sms.module';
+import { ConfigModule } from '@nestjs/config';
+import { emailConfig, smsConfig } from './config';
 
 @Module({
-  imports: [EmailModule, SmsModule],
+  imports: [
+    EmailModule,
+    SmsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [emailConfig, smsConfig],
+    }),
+  ],
   providers: [
     { provide: 'NotificationService', useClass: NotificationService },
   ],
